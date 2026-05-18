c12500dcc2e780190ffec8e5c3f7a4aab3eec1b7
MariuszF <mariusz.figiela.dev@gmail.com>
2026-05-18 21:04:19 +0200

    Implementation of Alembic

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,15 @@
-from sqlmodel import create_engine, SQLModel, Session
-from app.config import settings
+import os
+from sqlmodel import Session, create_engine
 
-engine = create_engine(settings.DATABASE_URL, echo=True)
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_SERVER = os.getenv("POSTGRES_SERVER")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
 
-def create_db_and_tables():
-    import app.models
-    SQLModel.metadata.create_all(engine)
+SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=False)
 
 def get_session():
     with Session(engine) as session:
